9acf5d3d211e1c0fe3b3463908d21cdacc0a32cd
David Hoang <88292464+David-Hoang@users.noreply.github.com>
2025-03-03 14:38:08 +0100

    Setup server port 8000 + create file

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
 import chalk from 'chalk';
+import http from 'http';
+import fs from 'fs';
 
-let students = [
-    'daphnée',
-    'lucie',
-    'maxime',
-    'toni'
-]
-
-let chalkColor = [
-    "green",
-    "blue",
-    "red",
-]
+let students = ['daphnée','lucie','maxime','toni']
+let chalkColor = ['green','blue','red']
 
 students.forEach(student => {
     let randomColor = chalkColor[Math.floor(Math.random() * chalkColor.length)];
     console.log(chalk[randomColor](student))
+})
+
+const server = http.createServer((request, response) => {
+    response.end('<h1>Hello World !</h1>')
+});
+
+const port = 8000;
+const domainName = "localhost"
+
+let path = process.cwd();
+let fileContent = "Hello World !";
+let fileName = "Hello-World";
+let fileExtension = "txt";
+
+fs.writeFile(`${path}\\${fileName}.${fileExtension}`, fileContent, (err) => {
+    if (err) console.log(err);
+    else {
+        console.log("Le fichier a été créer avec succès, le fichier contient : " + fs.readFileSync(`${path}\\${fileName}.${fileExtension}`, "utf8"));
+    }
+    });
+
+server.listen(port, domainName, () => {
+    console.log(`Le serveur tourne sur le port ${8000} !`);
 })
